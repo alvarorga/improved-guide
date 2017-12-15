2b96f9e16ea5b3a9fe1359f425ce5929d61f72c5
Alvaro R <alvarorga@hotmail.com>
2017-12-16 00:27:33 +0100

    Add structure to MPS class.

--- mpys/mps.py
+++ mpys/mps.py
@@ -2,5 +2,28 @@
 
 
 class Mps(object):
-    """Class for matrix product states (MPS)."""
-    pass
+    """Class for matrix product states (MPS).
+
+    Attributes:
+        L (int): length of the MPS.
+        D (int): maximum bond dimension.
+        M (list[arrays]): list with the tensors that define the MPS in
+            each site.
+    """
+    def __init__(self, L, name='None'):
+        """Initialize the MPS class.
+
+        Args:
+            L (int): length of the MPS.
+            name (str): name of the state that we initialize. Examples
+                are: 'GHZ' and 'AKLT'.
+        """
+        pass
+
+    def truncate(self, D_new):
+        """Truncate the MPS to a maximum bond dimension D_new.
+
+        Args:
+            D_new (int): maximum bond dimension of the truncated MPS.
+        """
+        pass
